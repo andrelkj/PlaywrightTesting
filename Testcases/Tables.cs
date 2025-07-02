e9bdba59efe67670af403ece53fc85ebdc4e2592
andrelkj <andre.kreutzer@outlook.com>
2025-07-02 01:57:23 -0300

    Add Tables.cs with examples of validating and iterating over table contents using Playwright

---
+++ Testcases/Tables.cs
@@ -0,0 +1,43 @@
+using Microsoft.Playwright;
+using NUnit.Framework;
+using static Microsoft.Playwright.Assertions;
+
+namespace PlaywrightTesting.Testcases;
+
+public class Tables
+{
+    static async Task Main(string[] args)
+    {
+        // Setup
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser =
+            await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+        var page = await browser.NewPageAsync();
+
+        // Navigation
+        await page.GotoAsync("https://money.rediff.com/indices/nse/NIFTY-50?5rc");
+
+        // Actions
+        // Row count
+        var row = page.Locator(".dataTable tbody tr");
+        Console.WriteLine(await row.CountAsync());
+
+        // Column count
+        var firstColumn = row.First.Locator("td");
+        Console.WriteLine(await firstColumn.CountAsync());
+
+        // Assertions
+        await Expect(firstColumn.Nth(1)).ToContainTextAsync("Nifty");
+        Console.WriteLine("Table content validation passed! ✅");
+
+        Console.WriteLine("---------------------------------------------------------------------");
+        
+        // Iterating over table lines to print their content
+        var allRowsContent = await row.AllTextContentsAsync();
+        
+        foreach (var rowContent in allRowsContent)
+        {
+            Console.WriteLine(rowContent);
+        }
+    }
+}

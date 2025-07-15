3fa2b3d860d416abd5a6aed97f6211de01bcd892
andrelkj <andre.kreutzer@outlook.com>
2025-07-14 22:44:28 -0300

    Enhance TestCase1.cs with setup and teardown methods; add ordered, categorized test cases with annotations

--- Nunit/TestCase1.cs
+++ Nunit/TestCase1.cs
@@ -4,15 +4,45 @@ namespace PlaywrightTesting.Nunit;
 
 public class TestCase1
 {
-    [Test]
+    [SetUp] // Executed before each test case
+    public void LaunchBrowser()
+    {
+        Console.WriteLine("Launching browser");
+    }
+
+    [TearDown] // Execute after each test case
+    public void CloseBrowser()
+    {
+        Console.WriteLine("Closing browser");   
+    }
+
+    [Test, Order(1), Category("smoke")] // Defines a test case of order 1 and type smoke
+    public void RegisterUser()
+    {
+        Console.WriteLine("Executing user registry test case");
+    }
+
+    [Test, Order(2), Category("smoke")] // Defines a test case of order 2 - will be executed after order 1
     public void DoLogin()
     {
         Console.WriteLine("Executing login test case");
     }
     
-    [Test]
-    public void RegisterUser()
+    [Test, Order(3), Category("bvt"), Ignore("Skip test case 3")] // Defines a test case of order 3 - will be executed after order 2
+    public void Test3()
     {
-        Console.WriteLine("Executing user registry test case");
+        Console.WriteLine("Executing test case 3");
+    }
+    
+    [Test, Order(4), Category("functional")] // Defines a test case of order 4 - will be executed after order 3
+    public void Test4()
+    {
+        Console.WriteLine("Executing test case 4");
+    }
+    
+    [Test, Order(5), Category("functional")] // Defines a test case of order 5 - will be executed after order 4
+    public void Test5()
+    {
+        Console.WriteLine("Executing test case 5");
     }
 }
